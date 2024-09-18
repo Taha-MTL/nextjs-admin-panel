ea49527c642118a8211246bd378b89b5497a7d68
Taha Bagwala <taha.bagwala@ment.tech>
2024-09-18 22:59:30 +0530

    updates

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -3,17 +3,6 @@ import React from "react";
 import ChatCard from "../Chat/ChatCard";
 import TableOne from "../Tables/TableOne";
 import DataStatsOne from "@/components/DataStats/DataStatsOne";
-import dynamic from "next/dynamic";
-const ChartOne = dynamic(() => import("@/components/Charts/ChartOne"), {
-  ssr: false,
-});
-const ChartTwo = dynamic(() => import("@/components/Charts/ChartTwo"), {
-  ssr: false,
-});
-const ChartThree = dynamic(() => import("@/components/Charts/ChartThree"), {
-  ssr: false,
-});
-const MapOne = dynamic(() => import("../Maps/MapOne"), { ssr: false });
 
 const ECommerce: React.FC = () => {
   return (
@@ -21,10 +10,6 @@ const ECommerce: React.FC = () => {
       <DataStatsOne />
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
-        <ChartOne />
-        <ChartTwo />
-        <ChartThree />
-        <MapOne />
         <div className="col-span-12 xl:col-span-8">
           <TableOne />
         </div>
